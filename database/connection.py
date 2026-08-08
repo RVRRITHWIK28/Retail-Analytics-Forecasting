6d78b3df042d1d38ac5adaf236a1eb693d1c1cd8
Rithwik <ramarithwik28@gmail.com>
2026-08-08 15:05:58 +0530

    Use environment variable for database connection

--- database/connection.py
+++ database/connection.py
@@ -1,6 +1,6 @@
-
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:9440211075%40Rv@localhost:5432/retail_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
